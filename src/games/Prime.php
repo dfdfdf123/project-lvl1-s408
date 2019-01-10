c900989cb09479f433d2583255f6d5715a386d3b
johnromanoff <johnromanoff@yandex.ru>
2019-01-10 23:21:23 +0300

    add brain-prime

---
+++ src/games/Prime.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace BrainGames\Games\Prime;
+
+use function \BrainGames\Cli\run;
+
+const DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+function isPrime($num)
+{
+    $squareRoot = sqrt($num);
+    for ($i = 2; $i < $squareRoot; $i +=1) {
+        if ($num % $i === 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function primeGame()
+{
+    $generateData = function () {
+        $randomNumber = rand(1, 100);
+        $correctAnswer = isPrime($randomNumber) ? 'yes' : 'no';
+        return [ 'question' => $randomNumber, 'correctAnswer' => $correctAnswer ];
+    };
+
+    return run($generateData, DESCRIPTION);
+}
