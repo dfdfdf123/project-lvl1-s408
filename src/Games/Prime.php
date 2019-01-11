afeaf0a29be2e6d61dfbc029566a3f531da69634
johnromanoff <johnromanoff@yandex.ru>
2019-01-11 15:02:02 +0300

    fix prime-game

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -8,7 +8,10 @@ const DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no
 
 function isPrime($num)
 {
-    for ($i = 2; $i < $num; $i += 1) {
+    if ($num < 2) {
+        return false;
+    }
+    for ($i = 2; $i <= $num / 2; $i += 1) {
         if ($num % $i === 0) {
             return false;
         }
